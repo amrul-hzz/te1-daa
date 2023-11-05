188b52b2b68ba5314e0f9f6c46d019566da79637
Amanda Nurul Izzah <108340923+amrul-hzz@users.noreply.github.com>
2023-11-05 22:29:59 +0700

    update

---
+++ Main.java
@@ -0,0 +1,174 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Arrays;
+
+public class Main {
+    static int[] small_randomized, small_sorted, small_reversed;
+    static int[] medium_randomized, medium_sorted, medium_reversed;
+    static int[] large_randomized, large_sorted, large_reversed;
+
+
+    public static void main(String[] args) {
+        int n_small = 512;
+        int n_medium = 8192;
+        int n_large = 65536;
+        long seed = 420L; // do not change seed
+
+//        // contoh penerapan
+//        int[] example = new int[]{1, 1, 9, 7 ,8, 10, 1, 2};
+//        System.out.print("Initial Array: ");
+//        printArray(example);
+//        RandomizedShellSort.randomizedShellSort(example);
+
+        generateNumbers(n_small,"small", seed);
+        generateNumbers(n_medium,"medium", seed);
+        generateNumbers(n_large,"large", seed);
+
+        evaluate();
+//        checkSorted();
+
+    }
+
+    public static void evaluate() {
+        // evaluate randomized shell sort
+        evaluateRandomShellSort(small_randomized, "small, random");
+        evaluateRandomShellSort(small_sorted, "small, sorted");
+        evaluateRandomShellSort(small_reversed, "small, reversed");
+
+        evaluateRandomShellSort(medium_randomized, "medium, random");
+        evaluateRandomShellSort(medium_sorted, "medium, sorted");
+        evaluateRandomShellSort(medium_reversed, "medium, reversed");
+
+        evaluateRandomShellSort(large_randomized, "large, random");
+        evaluateRandomShellSort(large_sorted, "large, sorted");
+        evaluateRandomShellSort(large_reversed, "large, reversed");
+
+        // evaluate max heap sort
+        evaluateMaxHeapSort(small_randomized, "small, random");
+        evaluateMaxHeapSort(small_sorted, "small, sorted");
+        evaluateMaxHeapSort(small_reversed, "small, reversed");
+
+        evaluateMaxHeapSort(medium_randomized, "medium, random");
+        evaluateMaxHeapSort(medium_sorted, "medium, sorted");
+        evaluateMaxHeapSort(medium_reversed, "medium, reversed");
+
+        evaluateMaxHeapSort(large_randomized, "large, random");
+        evaluateMaxHeapSort(large_sorted, "large, sorted");
+        evaluateMaxHeapSort(large_reversed, "large, reversed");
+    }
+    public static void checkSorted() {
+        // check if sorted
+        System.out.println(isSortedAscending(small_randomized));
+        System.out.println(isSortedAscending(small_sorted));
+        System.out.println(isSortedAscending(small_reversed));
+
+        System.out.println(isSortedAscending(medium_randomized));
+        System.out.println(isSortedAscending(medium_sorted));
+        System.out.println(isSortedAscending(medium_reversed));
+
+        System.out.println(isSortedAscending(large_randomized));
+        System.out.println(isSortedAscending(large_sorted));
+        System.out.println(isSortedAscending(large_reversed));
+    }
+    public static void evaluateRandomShellSort(int[] array, String label) {
+        System.gc();
+        Runtime runtime = Runtime.getRuntime();
+
+        double memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+        long startTime = System.nanoTime();
+
+        RandomizedShellSort.randomizedShellSort(array);
+
+        long endTime = System.nanoTime();
+        double memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+
+        long elapsedTimeNano = endTime - startTime;
+        double elapsedTimeMillis = (double) elapsedTimeNano / 1_000_000.0;
+        double usedMemory = (memoryAfter - memoryBefore)/1024;
+
+        System.out.println("Randomized Shell Sort - " + label + ": " + elapsedTimeMillis + ", " + usedMemory + " KB");
+    }
+    public static void evaluateMaxHeapSort(int[] array, String label) {
+        System.gc();
+        Runtime runtime = Runtime.getRuntime();
+
+        double memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+        long startTime = System.nanoTime();
+
+        MaxHeapSort.maxHeapSort(array);
+
+        long endTime = System.nanoTime();
+        double memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+
+        long elapsedTimeNano = endTime - startTime;
+        double elapsedTimeMillis = (double) elapsedTimeNano / 1_000_000.0;
+        double usedMemory = (memoryAfter - memoryBefore)/1024;
+
+        System.out.println("Max Heap Sort - " + label + ": " + elapsedTimeMillis + "ms, " + usedMemory + " KB");
+    }
+    public static void generateNumbers(int n, String s, long seed) {
+        Random random = new Random(seed);
+
+        int[] randomized = new int[n];
+        for (int i = 0; i < n; i++) {
+            randomized[i] = random.nextInt();
+        }
+        writeIntegersToFile(randomized, s + "_randomized.txt");
+
+        int[] sorted = randomized.clone();
+        Arrays.sort(sorted);
+        writeIntegersToFile(sorted, s + "_sorted.txt");
+
+        int[] reversed = new int[n];
+        for (int i = 0; i < n; i++) {
+            reversed[i] = sorted[n - 1 - i];
+        }
+        writeIntegersToFile(reversed, s + "_reversed.txt");
+
+        if (s.equals("small")){
+            small_randomized = randomized.clone();
+            small_sorted = sorted.clone();
+            small_reversed = reversed.clone();
+        }
+
+        if (s.equals("medium")){
+            medium_randomized = randomized.clone();
+            medium_sorted = sorted.clone();
+            medium_reversed = reversed.clone();
+        }
+
+        if (s.equals("large")){
+            large_randomized = randomized.clone();
+            large_sorted = sorted.clone();
+            large_reversed = reversed.clone();
+        }
+    }
+    public static void writeIntegersToFile(int[] integerList, String filePath) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
+            for (Integer number : integerList) {
+                bufferedWriter.write(number.toString());
+                bufferedWriter.newLine(); // Add a newline after each integer
+            }
+            System.out.println("Integers have been written to the file: " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean isSortedAscending(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void printArray(int[] arr){
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ RandomizedShellSort.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+/*
+ * Taken from:
+ * https://dl.acm.org/doi/pdf/10.1145/2049697.2049701?casa_token=Q3m5kUD8cXwAAAAA:W74e11vozONFwdODsiFG213H5SqLOzjM9a-bYPENfqnuP13NG5l3gZOMWCsKWgO1zKEZW8H47seNoA
+ */
+public class RandomizedShellSort {
+    public static final int C = 4; // number of region compare-exchange repetitions
+
+    public static void exchange(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void compareExchange(int[] a, int i, int j) {
+        if (((i < j) && (a[i] > a[j])) || ((i > j) && (a[i] < a[j]))) {
+            exchange(a, i, j);
+        }
+    }
+
+    public static void permuteRandom(int[] a, MyRandom rand) {
+        for (int i = 0; i < a.length; i++) {
+            exchange(a, i, rand.nextInt(a.length - i) + i);
+        }
+    }
+
+    public static void compareRegions(int[] a, int s, int t, int offset, MyRandom rand) {
+        int[] mate = new int[offset];
+        for (int count = 0; count < C; count++) {
+            for (int i = 0; i < offset; i++) mate[i] = i;
+            permuteRandom(mate, rand);
+            for (int i = 0; i < offset; i++) {
+                compareExchange(a, s + i, t + mate[i]);
+            }
+        }
+
+//        System.out.print("\t\tresult of compareRegion: ");
+//        Main.printArray(a);
+    }
+
+    public static void randomizedShellSort(int[] a) {
+        int n = a.length;
+        MyRandom rand = new MyRandom(); // assuming MyRandom is some custom random number generator
+
+        for (int offset = n / 2; offset > 0; offset /= 2) {
+            // compare-exhange up
+//            System.out.println("Offset = " + offset);
+            for (int i = 0; i < n - offset; i+= offset) {
+                compareRegions(a, i, i + offset, offset, rand);
+            }
+//            System.out.print("\tAfter compare-exchange up: ");
+//            Main.printArray(a);
+
+            // compare-exchange down
+            for (int i = n - offset; i >= offset; i -= offset) {
+                compareRegions(a, i - offset, i, offset, rand);
+            }
+//            System.out.print("\tAfter compare-exchange down: ");
+//            Main.printArray(a);
+
+            // compare 3 hops up
+            for (int i = 0; i < n - 3 * offset; i += offset) {
+                compareRegions(a, i, i + 3 * offset, offset, rand);
+            }
+//            System.out.print("\tAfter compare 3 hops up: ");
+//            Main.printArray(a);
+
+            // compare 2 hops up
+            for (int i = 0; i < n - 2 * offset; i += offset) {
+                compareRegions(a, i, i + 2 * offset, offset, rand);
+            }
+//            System.out.print("\tAfter compare 2 hops up: ");
+//            Main.printArray(a);
+
+            // compare odd-even regions
+            for (int i = 0; i < n; i += 2 * offset) {
+                compareRegions(a, i, i + offset, offset, rand);
+            }
+//            System.out.print("\tAfter compare odd-even regions: ");
+//            Main.printArray(a);
+
+            // compare even-odd regions
+            for (int i = offset; i < n - offset; i += 2 * offset) {
+                compareRegions(a, i, i + offset, offset, rand);
+            }
+//            System.out.print("\tAfter compare even-odd regions: ");
+//            Main.printArray(a);
+//
+//            System.out.println();
+        }
+    }
+
+}
+
+// Assuming MyRandom is some custom random number generator class
+class MyRandom {
+    Random r = new Random();
+
+    public int nextInt(int bound) {
+        return r.nextInt(bound);
+    }
+}
